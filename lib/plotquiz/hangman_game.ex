ff3ad6fb3f59c0c7e7c9cb13563ed5630323e9a3
ktunprasert <ktunprasert@outlook.com>
2023-09-18 20:25:07 +0100

    live_controller: moved into it's own module

---
+++ lib/plotquiz/hangman_game.ex
@@ -0,0 +1,100 @@
+defmodule PlotQuiz.HangmanGame do
+  defstruct [
+    :quiz,
+    :answer,
+    :hint,
+    :guesses,
+    :guess,
+    :seconds,
+    :t,
+    :timer,
+    :lives
+  ]
+
+  @seconds 5
+
+  # Starting a new game
+  def new_game(quiz) do
+    answer = quiz.name |> String.downcase()
+
+    %__MODULE__{
+      quiz: quiz,
+      answer: answer,
+      hint: update_hint(answer, MapSet.new()),
+      guesses: MapSet.new(),
+      seconds: @seconds,
+      t: timer_start(),
+      lives: min(15, String.length(answer) + 3)
+    }
+  end
+
+  def timer_tick(game) do
+    cond do
+      game.seconds == 0 ->
+        :timer.cancel(game.t)
+        %__MODULE__{game | seconds: @seconds, t: timer_start(), lives: game.lives - 1}
+
+      game.answer == game.hint ->
+        :timer.cancel(game.t)
+        %__MODULE__{game | seconds: @seconds}
+
+      true ->
+        %__MODULE__{game | seconds: game.seconds - 1}
+    end
+  end
+
+  def add_guess(game, guess) do
+    if is_legal(guess), do: update_guess(game, guess), else: game
+  end
+
+  def update_guess(game, guess) do
+    guesses = MapSet.put(game.guesses, guess)
+
+    lives =
+      cond do
+        guesses == game.guesses ->
+          game.lives
+
+        String.contains?(game.answer, guess) ->
+          game.lives
+
+        true ->
+          game.lives - 1
+      end
+
+    %__MODULE__{
+      game
+      | guesses: guesses,
+        lives: lives,
+        guess: guess,
+        hint: update_hint(game.answer, guesses)
+    }
+    |> dbg
+  end
+
+  defp update_hint(text, set) do
+    for letter <- text |> String.split("", trim: true) do
+      cond do
+        is_legal(letter) ->
+          if MapSet.member?(set, letter) do
+            letter
+          else
+            "_"
+          end
+
+        true ->
+          letter
+      end
+    end
+    |> dbg
+    |> Enum.join()
+  end
+
+  defp timer_start() do
+    :timer.send_interval(1_000, self(), :tick) |> elem(1)
+  end
+
+  def is_legal(char) do
+    char in ~w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+  end
+end
--- lib/plotquiz_web/controllers/test_controller.ex
+++ lib/plotquiz_web/controllers/test_controller.ex
@@ -2,10 +2,7 @@ defmodule PlotquizWeb.TestController do
   use Phoenix.LiveView
 
   alias Plotquiz.Movie
-  alias Plotquiz.Movie.Quiz
-
-  @guesses MapSet.new()
-  @time_between_guesses 5
+  alias PlotQuiz.HangmanGame
 
   def render(%{lives: 0} = assigns) do
     :timer.cancel(assigns.t)
@@ -19,18 +16,18 @@ defmodule PlotquizWeb.TestController do
     ~H"""
     <div phx-window-keyup="update_guess">
 
-    <pre class={"grid h-[40vh] place-items-center text-center tracking-[5px]"}><%= @hint %>
-    <code><%= @guesses |> MapSet.to_list() |> Enum.join(", ")  %></code>
+    <pre class={"grid h-[40vh] place-items-center text-center tracking-[5px]"}><%= @game.hint %>
+    <code><%= @game.guesses |> MapSet.to_list() |> Enum.join(", ")  %></code>
     </pre>
 
     <div class={"grid w-full place-items-center text-center"}>
-        <blockquote><%= @quiz.genres |> Enum.join(", ") %></blockquote>
+        <blockquote><%= @game.quiz.genres |> Enum.join(", ") %></blockquote>
 
-        Your guess [<%= @guess %>]
+        Your guess [<%= @game.guess %>]
         <br/>
-        Total lives: <%= @lives %>
+        Total lives: <%= @game.lives %>
         <br/>
-        Timer: <%= @timer %> seconds
+        Timer: <%= @game.seconds %> seconds
     </div>
 
     </div>
@@ -38,108 +35,17 @@ defmodule PlotquizWeb.TestController do
   end
 
   def mount(params, %{}, socket) do
-    quiz = Movie.get_quiz!(params["id"])
-
-    answer = quiz.name |> String.downcase()
-
-    {:ok,
-     assign(
-       socket,
-       hint: answer |> update_hint(@guesses),
-       answer: answer,
-       quiz: quiz,
-       guess: "",
-       guesses: @guesses,
-       timer: @time_between_guesses,
-       t: timer_start(),
-       lives: min(15, String.length(answer) + 3)
-     )}
+    game = HangmanGame.new_game(Movie.get_quiz!(params["id"]))
+    {:ok, assign(socket, game: game)}
   end
 
   def handle_info(:tick, socket) do
-    cond do
-      socket.assigns.timer == 0 ->
-        :timer.cancel(socket.assigns.t)
-
-        {:noreply,
-         assign(socket,
-           lives: socket.assigns.lives - 1,
-           timer: @time_between_guesses,
-           t: timer_start()
-         )}
-
-      socket.assigns.answer == socket.assigns.hint ->
-        :timer.cancel(socket.assigns.t)
-
-        {:noreply,
-         assign(socket,
-           lives: socket.assigns.lives,
-           timer: @time_between_guesses
-         )}
-
-      true ->
-        {:noreply, assign(socket, timer: socket.assigns.timer - 1)}
-    end
+    game = HangmanGame.timer_tick(socket.assigns.game) |> dbg
+    {:noreply, assign(socket, game: game)}
   end
 
   def handle_event("update_guess", %{"key" => value}, socket) do
-    if is_legal(value) do
-      {:noreply, update_guess(value, socket)}
-    else
-      {:noreply, socket}
-    end
-  end
-
-  defp timer_start() do
-    :timer.send_interval(1_000, self(), :tick) |> elem(1)
-  end
-
-  defp update_guess(value, socket) do
-    new_guesses =
-      socket.assigns.guesses
-      |> MapSet.put(value)
-
-    lives =
-      if new_guesses == socket.assigns.guesses do
-        socket.assigns.lives
-      else
-        socket.assigns.lives - 1
-      end
-
-    hint =
-      socket.assigns.answer
-      |> update_hint(new_guesses)
-
-    :timer.cancel(socket.assigns.t)
-
-    assign(socket,
-      hint: hint,
-      guesses: new_guesses,
-      guess: value,
-      timer: @time_between_guesses,
-      t: timer_start(),
-      lives: lives
-    )
-  end
-
-  defp update_hint(text, set) do
-    for letter <- text |> String.split("", trim: true) do
-      cond do
-        is_legal(letter) ->
-          if MapSet.member?(set, letter) do
-            letter
-          else
-            "_"
-          end
-
-        true ->
-          letter
-      end
-    end
-    |> Enum.join()
-  end
-
-  defp is_legal(char) do
-    char in ~w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+    game = HangmanGame.add_guess(socket.assigns.game, value)
+    {:noreply, assign(socket, game: game)}
   end
 end
